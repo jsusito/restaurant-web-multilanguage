edf9fcbd7b2b3186c770bea5baf9330231a27991
jsusito <109025737+jsusito@users.noreply.github.com>
2023-06-04 20:54:03 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   https: 'https://jsusito.github.io/restaurant-web-multilanguage/',
-  base: 'https://jsusito.github.io/restaurant-web-multilanguage/'
+  base: 'https://jsusito.github.io/restaurant-web-multilanguage/restaurante'
 })
