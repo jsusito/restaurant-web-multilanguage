b7fda411ba68ebd10e3958acff3eed8631983658
jsusito <109025737+jsusito@users.noreply.github.com>
2023-06-04 23:16:25 +0100

    update

--- src/config/i18n.jsx
+++ src/config/i18n.jsx
@@ -4,6 +4,8 @@ import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import I18NextHttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
+const getCurrentHost = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://jsusito.github.io/restaurant-web-multilanguage";
+
 i18
     .use(initReactI18next)
     .use(I18nextBrowserLanguageDetector)
@@ -11,5 +13,5 @@ i18
     .init(
         {
             backend: {
-                loadPath: "https://jsusito.github.io/restaurant-web-multilanguage/locales/{{lng}}/{{ns}}.json"
+                loadPath: `${getCurrentHost}/locales/{{lng}}/{{ns}}.json` 
             }})
